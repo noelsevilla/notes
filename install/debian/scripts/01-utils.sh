5797d50031a23fd85bf271b961224b90b8467d4a
noelsevilla <noel.sev@gmail.com>
2026-04-18 15:59:52 +1200

    Update debian install script for spotify

--- install/debian/scripts/01-utils.sh
+++ install/debian/scripts/01-utils.sh
@@ -70,9 +70,7 @@ sudo apt clean && \
 sudo snap install postman --channel=v9/stable
 
 # spotify
-curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg && \
-  echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list && \
-  sudo apt clean && \
-  sudo apt autoclean && \
+curl -sS https://download.spotify.com/debian/pubkey_5384CE82BA52C83A.asc | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg && \
+  echo "deb https://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list && \
   sudo apt update && \
   sudo apt install spotify-client
